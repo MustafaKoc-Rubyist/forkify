3ee20462f0f14f2d023fab2f16c949a8728dfbe3
OmarElsorogy <gneomar2021@gmail.com>
2021-03-18 18:55:00 +1300

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6cf91381-5fbb-4dfc-b7f6-cf7b2c17c6e5';
+export const MODAL_CLOSE_SEC = 2.5;
